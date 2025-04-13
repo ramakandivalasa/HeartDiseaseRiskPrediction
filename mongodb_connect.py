70bac4a717d8906ad4c300160c83fe44bcfca146
ramakandivalasa <155295434+ramakandivalasa@users.noreply.github.com>
2025-04-13 10:19:58 +0530

    Update mongodb_connect.py

--- mongodb_connect.py
+++ mongodb_connect.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 def get_db():
-    client = MongoClient("mongodb://localhost:27017/")
+    client = MongoClient("mongodb+srv://testuser:testpass123@cluster-1.n8r9dct.mongodb.net/")
     db = client["heart_disease_db"]
     return db["predictions"]  # Collection name
